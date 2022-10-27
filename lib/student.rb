c08520794b9f5161303924986a1035759853ca22
Ryan Canton <ryancanton999@gmail.com>
2022-10-27 09:06:52 -0600

    Initial commit: Create method and test for Student initialize method

---
+++ lib/student.rb
@@ -0,0 +1,14 @@
+class Student
+
+  attr_reader :name,
+              :age,
+              :scores
+
+  def initialize(info)
+    @name = info[:name]
+    @age = info[:age]
+    @scores = []
+  end
+
+
+end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,4 +1,15 @@
 require './lib/student'
 
 RSpec.describe Student do
+  it 'instantiates correctly' do
+    student = Student.new({name: "Morgan", age: 21})
+
+    expect(student).to be_instance_of(Student)
+    expect(student.name).to eq("Morgan")
+    expect(student.age).to eq(21)
+    expect(student.scores).to eq([])
+  end
+
+
+
 end
