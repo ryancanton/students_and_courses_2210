89e05b28eb4cc1ab2df0c43f0ea170262c55305a
Ryan Canton <ryancanton999@gmail.com>
2022-10-27 10:41:30 -0600

    Feat: Add method and test for list_students in class Gradebook

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -15,12 +15,19 @@ class Gradebook
   def list_courses
     courses_list = ""
     @courses.each do |course|
-      courses_list += "   #{course.name}: Capacity: #{course.capacity}/n"
+      courses_list += "   #{course.name}: Capacity: #{course.capacity}\n"
     end
-    courses_list.insert(0, "#{@instructor}'s Courses:/n")
+    courses_list.insert(0, "#{@instructor}'s Courses:\n")
   end
 
   def list_students
-
+    student_list = ""
+    @courses.each do |course|
+      student_list += "   #{course.name}:\n"
+      course.students.each do |student|
+        student_list += "      Name: #{student.name}, Age: #{student.age}, Grade: #{student.grade}\n"
+      end
+    end
+    student_list.insert(0, "#{@instructor}'s Students:\n")
   end
 end
--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -32,12 +32,36 @@ RSpec.describe Gradebook do
       course_2 = Course.new("Philosophy", 3)
       gradebook.add_course(course_1)
       gradebook.add_course(course_2)
-      
-      expect(gradebook.list_courses).to eq("Mr. Oz's Courses:/n" +
-                                           "   Calculus: Capacity: 2/n" +
-                                           "   Philosophy: Capacity: 3/n")
+
+      expect(gradebook.list_courses).to eq("Mr. Oz's Courses:\n" +
+                                           "   Calculus: Capacity: 2\n" +
+                                           "   Philosophy: Capacity: 3\n")
     end
   end
 
-
+  describe '#list_students' do
+    it 'correctly returns a list of students in courses' do
+      gradebook = Gradebook.new("Mr. Oz")
+      course_1 = Course.new("Calculus", 2)
+      course_2 = Course.new("Philosophy", 3)
+      student_1 = Student.new({name: "Morgan", age: 21})
+      student_2 = Student.new({name: "Grigor", age: 20})
+      student_3 = Student.new({name: "Bob", age: 21})
+      student_1.log_score(89)
+      student_2.log_score(76)
+      student_3.log_score(99)
+      course_1.enroll(student_1)
+      course_1.enroll(student_2)
+      course_2.enroll(student_3)
+      gradebook.add_course(course_1)
+      gradebook.add_course(course_2)
+      
+      expect(gradebook.list_students).to eq("Mr. Oz's Students:\n" +
+                                           "   Calculus:\n" +
+                                           "      Name: Morgan, Age: 21, Grade: 89.0\n" +
+                                           "      Name: Grigor, Age: 20, Grade: 76.0\n" +
+                                           "   Philosophy:\n" +
+                                           "      Name: Bob, Age: 21, Grade: 99.0\n")
+    end
+  end
 end
